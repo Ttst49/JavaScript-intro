6ca409066cf834275f991eb36ef47ec6c80ecb29
Ttst49 <thibautstachnick@gmail.com>
2022-10-10 13:00:05 +0200

    test

--- main.js
+++ main.js
@@ -9,9 +9,35 @@ let wordArray = word.split("")
 
 let resultatTrois = wordArray.splice(4,1,"")
 
-
+boom = wordArray.join("")
 
 console.log(resultat)
 console.log(secondResulat)
 console.log(wordArray)
+console.log(boom)
 
+const paragraphe =document.querySelector(".rouge")
+
+
+function mycall(){
+    paragraphe.classList.toggle('bleu')
+}
+
+    setInterval(mycall, 2000)
+
+
+let mots=["bonjour", "hello", "hola","你好"]
+
+function remplaceMots(mots){
+    for(i=0;i<=4;i++) {
+        if (i > 4) {
+            i = 0
+        }
+        paragraphe.replaceWith(mots[i])
+    }
+}
+
+setInterval(remplaceMots, 2000)
+
+
+//paragraphe.classList.toggle('bleu',)
